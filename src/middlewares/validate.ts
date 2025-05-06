461c6f7e67a4a3b0608efd0df59285af791cbb7a
Khoa <user.iamjasper@gmail.com>
2025-05-06 09:09:38 +0700

    feat: doi type validate

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
-import { AnyZodObject } from 'zod'
+import { ZodSchema } from 'zod'
 
-type ValidateSchema = Partial<Record<'body' | 'query' | 'params', AnyZodObject>>
-
-export const validate = (schema: ValidateSchema) => {
+export const validate = (schema: Record<string, ZodSchema>) => {
   return (req: Request, res: Response, next: NextFunction) => {
     for (const [key, validator] of Object.entries(schema)) {
       if (validator) validator.parse(req[key as keyof typeof req])
